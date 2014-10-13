36703df9799c3282226a2e3541268eab9dc8c4b8
javiersantos <javier.santos.vel@gmail.com>
2014-10-14 00:12:13 +0200

    Add support for search inputs

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,7 +1,7 @@
 document.addEventListener("keydown", function(e) {
 	var nodeName = e.target.nodeName.toLowerCase();
     if (e.which === 8) {
-    	if ((nodeName === 'input' && e.target.type === 'text') || nodeName === 'textarea' || (nodeName === 'input' && e.target.type === 'email') || (nodeName === 'input' && e.target.type === 'password')) {    
+    	if ((nodeName === 'input' && e.target.type === 'text') || nodeName === 'textarea' || (nodeName === 'input' && e.target.type === 'email') || (nodeName === 'input' && e.target.type === 'password') || (nodeName === 'input' && e.target.type === 'search')) {    
     	} else {
     		e.preventDefault();
     	}
