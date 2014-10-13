ca489930de703cc82750617ad0cb02733b7a5507
javiersantos <javier.santos.vel@gmail.com>
2014-10-13 23:52:59 +0200

    Add support for email and password inputs

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,7 +1,7 @@
 document.addEventListener("keydown", function(e) {
 	var nodeName = e.target.nodeName.toLowerCase();
     if (e.which === 8) {
-    	if ((nodeName === 'input' && e.target.type === 'text') || nodeName === 'textarea') {    
+    	if ((nodeName === 'input' && e.target.type === 'text') || nodeName === 'textarea' || (nodeName === 'input' && e.target.type === 'email') || (nodeName === 'input' && e.target.type === 'password')) {    
     	} else {
     		e.preventDefault();
     	}
